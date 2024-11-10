cbf74cc669f78186bb5b44223d77738722995666
Roman Stupak <stupak.roman.borisovich@gmail.com>
2024-11-10 02:32:24 -0600

    Wrapped code in command_line_interface.py with functions 'file_reading' and 'command_line_interface'

--- task_4_command_line_interface/command_line_interface.py
+++ task_4_command_line_interface/command_line_interface.py
@@ -3,39 +3,51 @@ import argparse
 # Imported function "one_char_counter" to count characters that don't repeat in the text
 from alone_standing_character import one_char_counter
 
-# Created a parser object with name "parser"
-parser = argparse.ArgumentParser(description="Returns the number of characters in the string occurring only once.")
+# Function that reads file with data
+def file_reading(path: str):
 
-# Created a "--string" command
-parser.add_argument('--string', type=str, help="Text for counting characters.")
+    # Try to open a file with data provided by useer
+    try:
+        if path:
+            with open(path, 'r') as file:
+                return file.read()
 
-# Created a "--file" command
-parser.add_argument('--file', type=str, help="Text from a file for counting characters.")
+    # If there's no such a file rise an ERROR
+    except FileNotFoundError:
+        return print("The file does not exist.")
 
-# Created an "args" object that works with arguments --string and --file
-args = parser.parse_args()
+# Function that creates a command line interface
+def command_line_interface():
 
-# Path to the file provided by user trough terminal
-file_path = args.file
+    # Created a parser object with name "parser"
+    parser = argparse.ArgumentParser(description="Returns the number of characters in the string occurring only once.")
 
-# Try to open a file with data provided by useer
-try:
-    if file_path:
-        with open(file_path, 'r') as file:
-            content = file.read()
+    # Created a "--string" command
+    parser.add_argument('--string', type=str, help="Text for counting characters.")
 
-# If there's no such a file rise an ERROR
-except FileNotFoundError:
-    print("The file does not exist.")
+    # Created a "--file" command
+    parser.add_argument('--file', type=str, help="Text from a file for counting characters.")
 
-# If user used only --string command
-if args.string and not args.file:
-    print(f"The amount of alone standing characters in string is {one_char_counter(args.string)}")
+    # Created an "args" object that works with arguments --string and --file
+    args = parser.parse_args()
 
-# If user used --string and --file command
-elif args.string and args.file:
-    print(f"The amount of alone standing characters in the string from the file is {one_char_counter(content)}")
+    # Path to the file provided by user trough terminal
+    file_path = args.file
 
-# If user used only --file command
-elif args.file and not args.string:
-    print(f"The amount of alone standing characters in the string from the file is {one_char_counter(content)}")
+    # String from the file if file existing
+    content = file_reading(file_path)
+
+    # If user used only --string command returns amount of characters that don't repeat
+    if args.string and not args.file:
+        return one_char_counter(args.string)
+
+    # If user used --string and --file command returns only amount of characters from the file that don't repeat
+    elif args.string and args.file:
+        return one_char_counter(content)
+
+    # If user used only --file command returns amount of characters from the file that don't repeat
+    elif args.file and not args.string:
+        return one_char_counter(content)
+
+if __name__ == "__main__":
+    command_line_interface()
