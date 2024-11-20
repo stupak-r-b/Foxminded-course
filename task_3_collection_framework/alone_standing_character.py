78549797c274e487fa836bc78816d8ba3a5e9911
Roman Stupak <stupak.roman.borisovich@gmail.com>
2024-11-20 01:05:55 -0600

    @lru_chace added instead of a simple dictionary

--- task_3_collection_framework/alone_standing_character.py
+++ task_3_collection_framework/alone_standing_character.py
@@ -1,30 +1,16 @@
-# By using 'shelve' module we can create a simple database
-import shelve
+from functools import lru_cache
 
-
-# A function that will count and return amount of  characters that don`t repeat
+# by using @lru_cache decorator we will improve function runtime by caching input and output
+@lru_cache(maxsize=100)
 def one_char_counter(text: str) -> int:
-
-    # Data base_dictionary. We're store here all the text and counter of characters that don't repeat, provided by user
-    shelf = shelve.open('mydata')
 
     # Raise an Error if input type is not a string, else continue
     if not isinstance(text, str):
         raise TypeError("Input must be a string!")
 
-    # Checking the "data_base" if user already provide text before
-    if text in shelf:
-        return shelf[text]
-
     # Here by using count method and for loop we can add characters to the list that repeats only once
     single_char_counter = [char for char in text if text.count(char) == 1]
 
-    # Add a new text that user provide for the first time in to a dictionary and amount of single characters in it
-    shelf[text] = len(single_char_counter)
-    return shelf[text]
-
-print(one_char_counter("How are you buddy?"))
-
-
-
+    # return amount of characters in the lis as a result
+    return len(single_char_counter)
 
