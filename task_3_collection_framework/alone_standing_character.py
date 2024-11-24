1e23e9e46566017e7b46ba6a2a32457262b66503
Roman Stupak <stupak.roman.borisovich@gmail.com>
2024-11-24 09:49:27 -0600

    Changed single_char_counter

--- task_3_collection_framework/alone_standing_character.py
+++ task_3_collection_framework/alone_standing_character.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 # by using @lru_cache decorator we will improve function runtime by caching input and output
 @lru_cache(maxsize=100)
-def one_char_counter(text: str): #-> int:
+def one_char_counter(text: str) -> int:
 
     # Raise an Error if input type is not a string, else continue
     if not isinstance(text, str):
@@ -13,7 +13,7 @@ def one_char_counter(text: str): #-> int:
     dict_of_characters = Counter(text)
 
     # Here by using count method and for loop we can add characters to the list that repeats only once
-    single_char_counter = [value for key, value in dict_of_characters.items() if dict_of_characters[key] == 1]
+    single_char_counter = [value for value in dict_of_characters.values() if value == 1]
 
     # return amount of characters in the lis as a result
     return len(single_char_counter)
